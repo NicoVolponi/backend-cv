f7535ad9ba26d46cad49f350f3fc82c5054261a2
NicoVolponi <nicovolpo9@gmail.com>
2026-01-10 22:55:26 -0300

    Update ProjectService.java

--- src/main/java/com/miportafolio/backend_cv/service/ProjectService.java
+++ src/main/java/com/miportafolio/backend_cv/service/ProjectService.java
@@ -14,13 +14,13 @@ public class ProjectService {
                         "Backend CV Dinámico",
                         "Proyecto personal para crear un CV dinámico con backend en Spring Boot y frontend en Vite/React.",
                         "http://localhost:5173", // link al frontend local (cuando esté corriendo con Vite)
-                        "" // el link al repo de GitHub lo agregamos cuando lo publiques
+                        "https://nico-cv.onrender.com/" // el link al repo de GitHub lo agregamos cuando lo publiques
                 ),
                 new Project(
                         "Berpic - Casa de Vinos",
                         "Desarrollo de una tienda online para la marca Berpic, enfocada en la venta de vinos, utilizando la plataforma Tienda Nube. Incluye personalización de plantillas, diseño responsive y optimización de la experiencia de compra.",
                         "https://berpicasadevinos.mitiendanube.com/", // link a la tienda online
-                        "" // cuando tengas el repo en GitHub lo agregamos
+                        "https://berpicasadevinos.mitiendanube.com/" // cuando tengas el repo en GitHub lo agregamos
                 )
         );
     }
