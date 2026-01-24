99a73a3f269cce5e634f6bfa5327f224f15d7c26
NicoVolponi <nicovolpo9@gmail.com>
2026-01-23 23:54:01 -0300

    Update AboutService.java

--- src/main/java/com/miportafolio/backend_cv/service/AboutService.java
+++ src/main/java/com/miportafolio/backend_cv/service/AboutService.java
@@ -10,11 +10,23 @@ public class AboutService {
         return new About(
                 "Nicolás Volponi",
                 "Técnico en Programación",
-                "Técnico en Programación con un perfil híbrido que combina habilidades técnicas y de gestión.
-Actualmente trabajo en Bits0, donde participo en proyectos sobre Salesforce Commerce Cloud, gestionando tanto la implementación técnica como la comunicación con los clientes.
-Este año inicié la Licenciatura en Tecnología de la Información, fortaleciendo mi formación académica y mi visión integral del desarrollo tecnológico.
-Además, continúo profundizando en el campo de la Inteligencia Artificial, abordando temas como machine learning, redes neuronales y agentes inteligentes, con el objetivo de integrar soluciones más innovadoras en cada proyecto.
-También realizo proyectos freelance, como la mejora de plataformas e-commerce en Tienda Nube, y disfruto asumir desafíos que mezclen tecnología, estrategia y creatividad."
+                """
+                Técnico en Programación con un perfil híbrido que combina habilidades técnicas y de gestión.
+
+                Actualmente trabajo en Bits0, participando en proyectos sobre Salesforce Commerce Cloud,
+                donde intervengo tanto en la implementación técnica como en la comunicación con clientes,
+                incluyendo proyectos para marcas como Lancôme y Kiehl’s Argentina.
+
+                Este año inicié la Licenciatura en Tecnología de la Información, fortaleciendo mi formación
+                académica y mi visión integral del desarrollo tecnológico.
+
+                En paralelo, continúo profundizando en Inteligencia Artificial, abordando conceptos como
+                machine learning, redes neuronales, IA generativa y agentes inteligentes, con el objetivo
+                de integrar soluciones innovadoras en cada proyecto.
+
+                Además, realizo proyectos freelance, como la mejora de plataformas e-commerce en Tienda Nube,
+                y disfruto asumir desafíos que combinan tecnología, estrategia y creatividad.
+                """
         );
     }
 }
