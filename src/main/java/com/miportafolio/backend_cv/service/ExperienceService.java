2640e43932914af21f7f64f5103b43a762d7f5c8
NicoVolponi <nicovolpo9@gmail.com>
2026-01-24 00:15:39 -0300

    Update ExperienceService.java

--- src/main/java/com/miportafolio/backend_cv/service/ExperienceService.java
+++ src/main/java/com/miportafolio/backend_cv/service/ExperienceService.java
@@ -11,12 +11,17 @@ public class ExperienceService {
     public List<Experience> getExperiences() {
         return List.of(
                 new Experience(
-                        "Account Manager",
+                        "Account Manager / Desarrollador Salesforce Commerce Cloud",
                         "Bits0",
                         "Mayo 2024 - Actualidad",
-                        "Gestión y organización de tareas junto al cliente, ejecución de implementaciones técnicas en Salesforce Commerce Cloud, control del proceso de QA y coordinación de salidas a producción (UAT)."
+                        "Gestión y organización de tareas junto al cliente, ejecución de implementaciones técnicas en Salesforce Commerce Cloud, control del proceso de QA y coordinación de salidas a producción (UAT). Participación en proyectos para marcas como Lancôme, Kiehl’s Argentina y Kérastase Chile."
+                ),
+                new Experience(
+                        "Desarrollador Web Freelance",
+                        "Freelance",
+                        "2024 - Actualidad",
+                        "Desarrollo y mejora de sitios web y aplicaciones, adaptando la solución según las necesidades del proyecto. Implementación mediante distintas plataformas y herramientas, así como desarrollo a medida con código. Enfoque en experiencia de usuario (UX), estructura, rendimiento y funcionalidades del sitio."
                 )
-                // más adelante podemos agregar más trabajos acá
         );
     }
 }
