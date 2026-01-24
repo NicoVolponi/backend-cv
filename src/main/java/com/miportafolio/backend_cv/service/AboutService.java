65d329ce2acb121361e39d8027f14877355d35f0
NicoVolponi <nicovolpo9@gmail.com>
2026-01-24 00:47:06 -0300

    Update AboutService.java

--- src/main/java/com/miportafolio/backend_cv/service/AboutService.java
+++ src/main/java/com/miportafolio/backend_cv/service/AboutService.java
@@ -22,9 +22,9 @@ public class AboutService {
                 combinando código, criterio funcional y experiencia de usuario.
                 """,
                 List.of(
-                        "Salesforce Commerce Cloud: Kiehl’s AR, Lancôme AR, Kérastase CL",
-                        "Desarrollo full stack con Java, Spring Boot y React",
-                        "Gestión técnico–funcional de proyectos digitales"
+                        "Salesforce Commerce Cloud: Kiehl’s y Lancôme Argentina y Kérastase Chile",
+                        "Desarrollo Full Stack enfocado en producto y experiencia",
+                        "Gestión técnica de proyectos digitales"
                 ),
                 "NV"
         );
